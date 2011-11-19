984bbde00b6d9a7b75f23585784dbd61851283fe
deepakkumarnd@gmail.com <deepak@deep-sys.(none)>
2011-11-19 16:07:20 +0530

    Added Graph and random samples, generate a random string
    a	../pkg/

---
+++ lib/graph.rb
@@ -0,0 +1,12 @@
+# Basic graph algorithm implementation.\
+# creating a graph
+# deleting a node
+# inserting a node
+# search dfs, nfs
+# shortest path between two nodes
+# 
+
+module Graph
+
+
+end
---
+++ lib/random_samples.rb.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+def generate_random_key lenght=8
+  str = ''
+  length.times{ str<< (48+rand(43)).chr }
+end
